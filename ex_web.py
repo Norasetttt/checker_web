4d7b4cfd7f4c286dddcb3e6c573df1e6a77b22c8
[Giont4] <[47giant@gmail.com]>
2023-06-28 18:30:00 +0700

    add new df

--- ex_web.py
+++ ex_web.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 df = pd.read_csv("all_file.csv")
-
+new_df = df.set_index('start_time')
 
 def df_to_table() :
     
@@ -14,5 +14,7 @@ st.video("https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygULcmljayBhc3RsZXk%3Dl
 st.title("My Streamlit App")
 st.write("Welcome to my first Streamlit app!")
 st.experimental_data_editor(df)
+st.experimental_data_editor(new_df)
 if st.button("submit"):
     st.write('submitted')
+
