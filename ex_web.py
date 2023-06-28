d4a75b164b9ab32e97a2821a0b98510359d4ef2b
[Giont4] <[47giant@gmail.com]>
2023-06-28 14:14:16 +0700

    df

--- ex_web.py
+++ ex_web.py
@@ -6,12 +6,11 @@ import pandas as pd
 
 # Read the CSV file into a DataFrame
 df = pd.read_csv("all_file.csv")
-scrollable_content  = '<div style="height: 10px; overflow: scroll;">'+df['answer'][0]+ '</div>' 
-df.at[0, 'answer'] = scrollable_content
+
 
 st.video("https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygULcmljayBhc3RsZXk%3Dl")
 st.title("My Streamlit App")
 st.write("Welcome to my first Streamlit app!")
-st.table(df)
+st.dataframe(df)
 if st.button("submit"):
     st.write('submitted')
