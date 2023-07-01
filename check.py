0e7e522dd6baeb180be3b6ebc2d4e22439562cf6
[Giont4] <[47giant@gmail.com]>
2023-07-01 14:06:46 +0700

    mod multipage

--- check.py
+++ check.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import os
+import random
 
 df = pd.read_csv(r"C:\Users\47gia\Downloads\create_web\all_file.csv")
 
@@ -10,9 +10,6 @@ for i in range(1, len(df["File Name"])):
     if df["File Name"][i - 1] != df["File Name"][i]:
         yid_list.append(df["File Name"][i])
 
-
-
-
 def convert_time_to_seconds(time):
     h, m, s = map(int, time.split(':'))
     return h * 3600 + m * 60 + s
@@ -20,30 +17,42 @@ def convert_time_to_seconds(time):
 def get_row(df,row):
     return df.iloc[row,0:2]
 
+def get_unique_keys(number) :
+    keys = []
+    for i in range(number) :
+        random_number = random.randint(1,50000)
+        if random_number not in keys :
+            keys.append(random_number)
+    return keys 
 
-def create_pages(yid_list) :
+def create_pages(yid_list):
     pages = {}
-    yid_pages = [yid+'_page' for yid in yid_list]
-    for yid in yid_list :
-        for page in yid_pages:        
-            def page () :
+    yid_pages = [yid + '_page' for yid in yid_list]
+    keys = get_unique_keys(1000)
+    for yid, page_key in zip(yid_list, yid_pages):
+        def create_page(yid_value):
+            def page():
                 indices = []
-                for id in list(df["File Name"]) :
-                    if id == yid :
-                        indices.append(list(df["File Name"]).index(id))
-                for i in range(min(indices),max(indices)+1) :
-                    st.title("start time "+str(df["start_time"][i]))
-                    if st.button("Jump to start"):
-                        video = st.video("https://www.youtube.com/watch?v="+yid , start_time = convert_time_to_seconds(df["start_time"][i]))  
-                    st.data_editor(get_row(df,i)) 
-            pages[yid] = page
-    return pages 
+                for i, id in enumerate(list(df["File Name"])):
+                    if id == yid_value:
+                        indices.append(i)
+                for i in range(min(indices), max(indices) + 1):
+                    random_key = random.choice(keys)
+                    st.title("start time " + str(df["start_time"][i]))
+                    if st.button("Jump to start",key = random_key ):
+                        video = st.video("https://www.youtube.com/watch?v=" + yid_value, start_time=convert_time_to_seconds(df["start_time"][i]))
+                    st.data_editor(get_row(df, i))
+                    keys.remove(random_key)
+            return page
+
+        pages[page_key] = create_page(yid)
+
+    return pages
 
 
-
-pages = create_pages(yid_list)
 
 def main():
+    
     st.sidebar.title("Navigation")
     selection = st.sidebar.radio("Go to", list(pages.keys()))
     page = pages[selection]
--- ex_web.py
+++ ex_web.py
@@ -2,13 +2,15 @@ import streamlit as st
 import pandas as pd
 
 
-df = pd.read_csv("multipage/all_file.csv")
+df = pd.read_csv("all_file.csv")
 new_df = df.set_index('start_time')
 
+
+#create list of youtube id               
 yid_list = [df["File Name"][0]]
 for i in range(1, len(df["File Name"])):
     if df["File Name"][i - 1] != df["File Name"][i]:
-        yid_list.append(df["File Name"][i].strip())
+        yid_list.append(df["File Name"][i])
 
 def convert_time_to_seconds(time):
     h, m, s = map(int, time.split(':'))
@@ -17,30 +19,42 @@ def convert_time_to_seconds(time):
 def get_row(df,row):
     return df.iloc[row,0:2]
 
+def get_unique_keys(number) :
+    keys = []
+    for i in range(number) :
+        random_number = random.randint(1,50000)
+        if random_number not in keys :
+            keys.append(random_number)
+    return keys 
 
-def create_pages(yid_list) :
+def create_pages(yid_list):
     pages = {}
-    yid_pages = [yid+'_page' for yid in yid_list]
-    for yid in yid_list :
-        for page in yid_pages:        
-            def page () :
+    yid_pages = [yid + '_page' for yid in yid_list]
+    keys = get_unique_keys(1000)
+    for yid, page_key in zip(yid_list, yid_pages):
+        def create_page(yid_value):
+            def page():
                 indices = []
-                for id in list(df["File Name"]) :
-                    if id == yid :
-                        indices.append(list(df["File Name"]).index(id))
-                for i in range(min(indices),max(indices)+1) :
-                    st.title("start time "+str(df["start_time"][i]))
-                    if st.button("Jump to start"):
-                        video = st.video("https://www.youtube.com/watch?v="+yid , start_time = convert_time_to_seconds(df["start_time"][i]))  
-                    st.data_editor(get_row(df,i)) 
-            pages[yid] = page
-    return pages 
+                for i, id in enumerate(list(df["File Name"])):
+                    if id == yid_value:
+                        indices.append(i)
+                for i in range(min(indices), max(indices) + 1):
+                    random_key = random.choice(keys)
+                    st.title("start time " + str(df["start_time"][i]))
+                    if st.button("Jump to start",key = random_key ):
+                        video = st.video("https://www.youtube.com/watch?v=" + yid_value, start_time=convert_time_to_seconds(df["start_time"][i]))
+                    st.data_editor(get_row(df, i))
+                    keys.remove(random_key)
+            return page
+
+        pages[page_key] = create_page(yid)
+
+    return pages
 
 
-
-pages = create_pages(yid_list)
 
 def main():
+    pages = create_pages(yid_list)
     st.sidebar.title("Navigation")
     selection = st.sidebar.radio("Go to", list(pages.keys()))
     page = pages[selection]
@@ -48,6 +62,3 @@ def main():
 
 if __name__ == "__main__":
     main()      
-
-
-
