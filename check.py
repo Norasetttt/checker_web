8e8dec07c4de35ad65b81b5e76e43ec66820cd72
[Giont4] <[47giant@gmail.com]>
2023-07-04 17:39:41 +0700

    try new method

--- check.py
+++ check.py
@@ -1,7 +1,37 @@
 import streamlit as st
+import pandas as pd
+df = pd.read_csv(r"C:\Users\47gia\Downloads\create_web\all_file.csv")
 
-def on_upper_clicked():
-    st.session_state.text = st.session_state.text.upper()
+yid_list = [df["File Name"][0]]
+for i in range(1, len(df["File Name"])):
+    if df["File Name"][i - 1] != df["File Name"][i]:
+        yid_list.append(df["File Name"][i])
 
-st.text_area("Enter text", key="text")
-st.button("Upper Text", on_click=on_upper_clicked)
+
+
+def create_pages(yid_list):
+    pages = {}
+    yid_pages = [yid for yid in yid_list] #change to youtube name
+
+    #create dict of pages {page name : function} (function contain page's details)
+    for yid, page_key in zip(yid_list, yid_pages):
+        #create one page
+        def create_page(i):
+            def page():
+                st.title(str(df["start_time"][i]) + '  to  ' + str(df["end_time"][i]))
+                st.text_input("question",value = str(df["question"][i]))
+                st.text_input("answer",value = str(df["answer"][i]))
+                return page
+            
+            #get row and column of youtube_id's q&a from dataframe
+        indices = [] 
+        for i, id in enumerate(list(df["File Name"])):
+            if id == yid:
+                indices.append(i)
+        pages[page_key] = []
+        for i in range(min(indices),max(indices)+1) :
+            pages[page_key].append(create_page(i))
+
+    return pages
+
+print(create_pages(yid_list))
--- ex_web.py
+++ ex_web.py
@@ -31,59 +31,41 @@ def get_unique_keys(number) :
 # create a mutiple pages with list of youtube-id /// return a dictionary with youtube-id as a key and function that contain detail of each youtube-id as a value 
 def create_pages(yid_list):
     pages = {}
-    yid_pages = [yid for yid in yid_list] #change to youtube name 
-    keys = get_unique_keys(1000)  #generate keys
-    
+    yid_pages = [yid for yid in yid_list] #change to youtube name
+
     #create dict of pages {page name : function} (function contain page's details)
     for yid, page_key in zip(yid_list, yid_pages):
         #create one page
-        def create_page(yid):
+        def create_page(i):
             def page():
-                #get row and column of youtube_id's q&a from dataframe
-                indices = [] 
-                for i, id in enumerate(list(df["File Name"])):
-                    if id == yid:
-                        indices.append(i)
-                
-                #write the q&a on the page
-                for i in range(min(indices), max(indices) + 1):
-                    random_key = random.choice(keys)
-                    st.title(str(df["start_time"][i]) + '  to  ' + str(df["end_time"][i]))
-                    st.text_input("question",value = str(df["question"][i]))
-                    st.text_input("answer",value = str(df["answer"][i]))
-                    back,submit,next = st.columns(3)
-                    with back :
-                        if st.button("Back"):
-                            st.text('')
-                    with submit :
-                        if st.button("Submit"):
-                            st.text('')
-                    with next :
-                        if st.button("Next") :
-                            st.text('')
-                            
-
-
-                    keys.remove(random_key)
-            return page
-
-        pages[page_key] = create_page(yid)
+                st.title(str(df["start_time"][i]) + '  to  ' + str(df["end_time"][i]))
+                st.text_input("question",value = str(df["question"][i]))
+                st.text_input("answer",value = str(df["answer"][i]))
+                return page
+            
+            #get row and column of youtube_id's q&a from dataframe
+        indices = [] 
+        for i, id in enumerate(list(df["File Name"])):
+            if id == yid:
+                indices.append(i)
+        pages[page_key] = []
+        for i in range(min(indices),max(indices)+1) :
+            pages[page_key].append(create_page(i))
 
     return pages
 
 
-#start  \to build a web with csv dataframe
+#start to build a web with csv dataframe
 def main():
     pages = create_pages(yid_list)
+    print(pages)
     st.sidebar.title("Navigation")
     selection = st.sidebar.radio("Go to", list(pages.keys()))
-    page = pages[selection]
+    page = pages[selection][0]
     page()
+    
 
 if __name__ == "__main__":
     main()      
 
 
-st.text_area("something",placeholder='???')
-
-st.text_input("question",value='???')
