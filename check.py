2a50eca519f7d87e01d97d533be9ae5b7a014bbd
[Giont4] <[47giant@gmail.com]>
2023-07-06 15:07:58 +0700

    debug

--- check.py
+++ check.py
@@ -9,29 +9,30 @@ for i in range(1, len(df["File Name"])):
 
 
 
+# create page's appearance // i = index of interest row
+def create_page(i):
+    def page():
+        st.title(str(df["start_time"][i]) + '  to  ' + str(df["end_time"][i]))
+        st.text_input("question", value=str(df["question"][i]))
+        st.text_input("answer", value=str(df["answer"][i]))
+    return page
+
+# create a multiple pages with list of youtube-id /// return a dictionary with youtube-id as a key and function that contain details of each youtube-id as a value 
 def create_pages(yid_list):
     pages = {}
-    yid_pages = [yid for yid in yid_list] #change to youtube name
-
-    #create dict of pages {page name : function} (function contain page's details)
+    yid_pages = [yid for yid in yid_list]
+    # create dict of pages {page name : function} (function containing page's details)
     for yid, page_key in zip(yid_list, yid_pages):
-        #create one page
-        def create_page(i):
-            def page():
-                st.title(str(df["start_time"][i]) + '  to  ' + str(df["end_time"][i]))
-                st.text_input("question",value = str(df["question"][i]))
-                st.text_input("answer",value = str(df["answer"][i]))
-            return page()
-            
-            #get row and column of youtube_id's q&a from dataframe
-        indices = [] 
+        # create one page
+        # get row and column of youtube_id's q&a from dataframe
+        indices = []
         for i, id in enumerate(list(df["File Name"])):
             if id == yid:
                 indices.append(i)
         pages[page_key] = []
-        for i in range(min(indices),max(indices)+1) :
+        for i in range(min(indices), max(indices) + 1):
             pages[page_key].append(create_page(i))
-
     return pages
 
-print(len(create_pages(yid_list)['61oREuQ5JU8']))
+
+print(create_pages(yid_list))
--- ex_web.py
+++ ex_web.py
@@ -28,32 +28,30 @@ def get_unique_keys(number) :
             keys.append(random_number)
     return keys 
 
-# create a mutiple pages with list of youtube-id /// return a dictionary with youtube-id as a key and function that contain detail of each youtube-id as a value 
+# create page's appearance // i = index of interest row
+def create_page(i):
+    def page():
+        st.title(str(df["start_time"][i]) + '  to  ' + str(df["end_time"][i]))
+        st.text_input("question", value=str(df["question"][i]))
+        st.text_input("answer", value=str(df["answer"][i]))
+    return page
+
+# create a multiple pages with list of youtube-id /// return a dictionary with youtube-id as a key and function that contain details of each youtube-id as a value 
 def create_pages(yid_list):
     pages = {}
-    yid_pages = [yid for yid in yid_list] #change to youtube name
-
-    #create dict of pages {page name : function} (function contain page's details)
+    yid_pages = [yid for yid in yid_list]
+    # create dict of pages {page name : function} (function containing page's details)
     for yid, page_key in zip(yid_list, yid_pages):
-        #create one page
-        def create_page(i):
-            def page():
-                st.title(str(df["start_time"][i]) + '  to  ' + str(df["end_time"][i]))
-                st.text_input("question",value = str(df["question"][i]))
-                st.text_input("answer",value = str(df["answer"][i]))
-            return page()
-            
-            #get row and column of youtube_id's q&a from dataframe
-        indices = [] 
+        # create one page
+        # get row and column of youtube_id's q&a from dataframe
+        indices = []
         for i, id in enumerate(list(df["File Name"])):
             if id == yid:
                 indices.append(i)
         pages[page_key] = []
-        for i in range(min(indices),max(indices)+1) :
+        for i in range(min(indices), max(indices) + 1):
             pages[page_key].append(create_page(i))
-
     return pages
-
 
 #start to build a web with csv dataframe
 def main():
