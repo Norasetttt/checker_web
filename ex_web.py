db9e78fa3408bd1d5db7df5aded809f80e62d9dc
[Giont4] <[47giant@gmail.com]>
2023-07-10 13:09:13 +0700

    try

--- ex_web.py
+++ ex_web.py
@@ -60,40 +60,24 @@ def main():
     pages = create_pages(yid_list)
     st.sidebar.title("Navigation")
     selection = st.sidebar.radio("Go to", list(pages.keys()))
-    global i
     i = 0 
-    back, submit, next = st.columns(3)
-
-    def next_button():
-        with next:
-            if st.button("Next"):
-                i += 1
-
-    def submit_button():
-        with submit:
-            if st.button("Submit"):
-                st.title("Submitted")
-
-    def back_button():
-        with back:
-            if st.button("Back"):
+    while i < len(pages[selection]) :
+        page = pages[selection][i]
+        page()
+        back, submit, next = st.columns(3)
+        page = pages[selection][i]
+        
+        if i > 0 :
+            if back.button("Back"):
                 i -= 1
-
-    if i == 0:
-        submit_button()
-        next_button()
-    elif i == pages[selection][-1].index():
-        back_button()
-        submit_button()
-    else:
-        back_button()
-        submit_button()
-        next_button()
-
-    page = pages[selection][i]
-    page()  
-
-    
+        
+        if submit.button("Submit"):
+            st.title("Submitted")
+        
+        if i < pages[selection][-1]:
+            if next.button("Next"):
+                i += 1
+            
 if __name__ == "__main__":
     main()      
 
